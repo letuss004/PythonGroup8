e086c4a9508a97d26675ea6fb3bf84779851441e
Le Anh Tu <letuss004@gmail.com>
2021-04-19 00:29:52 +0700

    Delete

--- B4.py
+++
@@ -1,8 +0,0 @@
-#Nhap gia tri tu ban phim va chuyen sang kieu so nguyen
-soThapPhan = int(input())
-
-#Xuat cau voi dinh dang theo yeu cau de bai
-# %d dong vai tro giu cho cho 1 gia tri so thap phan
-# %o dong vai tro giu cho cho 1 gia tri so bat phan
-
-print('So thap phan %d trong he bat phan la %o' % (soThapPhan, soThapPhan))
